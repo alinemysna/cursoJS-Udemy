f8aff0c5b5fd4ba5bf083322a5da4ff856a33816
Aline Mysna <ianlucasgame7@gmail.com>
2023-10-16 19:24:47 -0300

    operadores aritmeticos

---
+++ aula09/tests.js
@@ -0,0 +1,13 @@
+const nome ='Lucas' //string
+const nome1 = "Ian" //string
+const nome2 = `Lima` //string
+
+const num1 =10 //number
+const num2 = 10.52 //number
+let nomeAluno //undefined = não aponta pra local nenhum ns memória
+let SobrenomeAluno = null //Nulo -. não aponta pra local nenhum na memória
+
+const aprovado = true // boolean = true , false (LÓGICO)
+
+console.log(typeof aprovado, aprovado)
+
---
+++ aula10/index.js
@@ -0,0 +1,12 @@
+/*
+* Aritméticos
+* + Adição / Concatenação
+*  - / *
+* ** potenciação
+* % resto da divisão
+*/
+
+const num1 = 10
+const num2 = 3 
+
+console.log(num1 % num2)
