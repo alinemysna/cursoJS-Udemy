697960915ef1c2641ce6bc0c4b7c8efa5c2bec3e
Aline Mysna <ianlucasgame7@gmail.com>
2023-10-24 19:33:24 -0300

    Funções- Básico

---
+++ js/aula17/tests.js
@@ -0,0 +1,29 @@
+/*function saudacao(nome) {
+  return `Bom dia ${nome}!`
+}
+
+ const variavel = saudacao('Aline')
+ console.log(variavel)
+
+
+function soma(x, y) {
+    const resultado = x + y
+    return resultado
+}
+console.log(soma(2, 2))
+console.log(soma(3, 2))
+
+
+const raiz = (n) => {   //esse sinal de igual e seta (=>) representa uma erow function
+  return n** 0.5 //raiz quadrada
+}; //quando uma variavel recebe uma funçao eu tenho que colocar ponto e virgula
+
+const raiz = n => n**0.5 // simplificação da funçao
+*/
+const raiz = function(n) {   
+    return n** 0.5 
+  }
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+
