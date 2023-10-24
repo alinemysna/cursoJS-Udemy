1319552a2862b3a19621efbf66cead018629aaa2
Aline Mysna <ianlucasgame7@gmail.com>
2023-10-23 21:00:53 -0300

    arrays básico

---
+++ js/aula16/index.js
@@ -0,0 +1,45 @@
+/*            123456789   assim por diante
+const nome = 'Aline Mysna' 
+
+console.log(alunos )
+console.log(alunos [0])
+console.log(alunos [2]) 
+alunos[0] = 'Eduardo' //muda o item array
+alunos[3] = 'Luiza'  //adiciona
+
+alunos[alunos.length] = 'Luiza' colocar mais itens no final do array sem saber o tamnho do array
+alunos[alunos.length] = 'Fábio'
+alunos[alunos.length] = 'Luana'
+
+alunos.push('Luiz')     adiciona no fim
+alunos.push('Fábio')
+
+alunos.unshift('Luiza') adiciona no inicio
+alunos.unshift('Fábio')
+
+const removido = alunos.pop()   remove o ultimo
+const removido = alunos.shift()  remove o primeiro
+console.log(removido)
+
+console.log(alunos[1]) // > undefined
+console.log(alunos) // exclui o item mais nao altera o array
+delete alunos[1]
+
+console.log(alunos[2]) qual elemento você quer pegar
+console.log(alunos.slice(0, 3)) de quanto a quanto vc quer pegar / pegar uma parte do array
+console.log(alunos.slice(0, -2)) remove a quantidade de elementos 
+
+console.log(typeof alunos)   > object
+
+console.log(alunos instaceof Array) > estar perguntado se é uma array true ou false
+
+
+*/
+//               1       2        3    cada elemento
+const alunos = ['Luiz', 'Maria', 'João']
+
+
+
+console.log(alunos)
+
+
