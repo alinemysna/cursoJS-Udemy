3b4af5ed075c6d2a1674f77286a3dd9e8a42b57c
Aline Mysna <ianlucasgame7@gmail.com>
2023-10-30 19:05:12 -0300

    operdores Lógicos e de comparação

---
+++ js/aula21/index.js
@@ -0,0 +1,15 @@
+/*
+operadores de comparação
+> maior que
+>= maior que ou igual q
+< menor que
+<= menor que ou igual a
+== igual (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = 10;
+const comp = num1 !== num2;
+console.log(comp);
---
+++ js/aula22/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+&& -> AND -> E >    Todas expressões tem que ser verdadeiras
+|| -> OR -> OU
+!  -> NOT -> NÃO
+
+ //const expressaoAnd = (true && true && true && true);
+ //const expressaoOr = false || true || true || true;
+ //console.log(expressaoOr);
+
+ const usuario = 'Aline';
+ const senha = '123456';
+
+ const vaiLogar = usuario ==='Aline' ||  senha === '12345';
+ console.log(vaiLogar);
+*/
+console.log(false)
