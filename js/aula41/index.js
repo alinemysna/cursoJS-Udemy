bd16d4966fa605c9f4623683b804ee03406ef3c4
Aline Mysna <ianlucasgame7@gmail.com>
2023-12-01 16:01:22 -0300

    lógica de programação - 01

---
+++ js/aula41/index.js
@@ -0,0 +1,10 @@
+//escreva uma função que recebe 2 numeros e retorna o maior deles.
+
+// function max(x, y) {
+//      return x > y ? x : y;
+// }
+
+const max2 = (x, y) => x > y ? x : y;
+
+  //const maior = max(1, 2);
+  console.log(max2(10, 20));
