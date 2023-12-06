1b9316b28deb9cdfc64c1445a001cd3340b68f98
Aline Mysna <ianlucasgame7@gmail.com>
2023-12-06 14:19:47 -0300

    exercício de lógica de programação-02

---
+++ js/aula42/index.js
@@ -0,0 +1,19 @@
+// Escreva uma função ePaisagem que
+// recebe dois argumentos, largura e altura
+// de uma imagem (number)
+// retorne true se a imagem estiver no modo
+// paisagem
+
+
+
+// function ePaisagem (largura, altura) {
+//  return largura > altura;
+// }
+
+// console.log(ePaisagem(1980, 1080));
+
+const ePaisagem = (largura, altura) => largura > altura;
+   
+   console.log(ePaisagem(1980, 1080));
+   
+
