1618b892a2da667aa4e1cb1e387b02cab0f9a422
Aline Mysna <ianlucasgame7@gmail.com>
2023-12-18 14:26:26 -0300

    As várias maneiras de declarar funções em JavaScript

---
+++ js/aula49/index.js
@@ -0,0 +1,32 @@
+//Declaração de função (Function hoisting)
+FalaOi();
+function FalaOi() {
+    console.log('Oie');
+}
+
+//First-class objects (Objetos de primeira classe)
+//Function expression
+const souUmDado = function() {
+    console.log('Sou um dado');
+}
+souUmDado();
+
+function executaFuncao(funcao) {
+    console.log('Vou executar sua função abaixo:')
+    funcao();
+}
+executaFuncao(souUmDado);
+
+//Arrow function 
+const funcaoArrow = () => {
+    console.log('Sou uma Arrow function');
+};
+funcaoArrow();
+
+//Dentro de um objeto
+const obj = {
+    falar() {
+        console.log('Estou falando...');
+    }
+};
+obj.falar();
