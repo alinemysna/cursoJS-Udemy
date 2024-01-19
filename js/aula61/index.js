5fb09a9cf51e660b0b22d39c93e8c350c8167c8e
Aline Mysna <ianlucasgame7@gmail.com>
2024-01-19 17:57:33 -0300

    Funções geradoras

---
+++ js/aula61/index.js
@@ -0,0 +1,67 @@
+function* geradora1() {
+    yield 'Valor 1';
+
+    yield 'Valor 2';
+
+    yield 'Valor 3';
+}
+
+// const g1 = geradora1();
+// for(let valor of g1) {
+//     console.log(valor);
+// }
+
+function* geradora2() {
+    let i = 0
+
+    while(true) {
+        yield i;
+        i++;
+    }
+}
+
+// const g2 = geradora2();
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+// console.log(g2.next().value);
+
+function* geradora3() {
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4() {
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+// for(let valor of g4) {
+//     console.log(valor);
+// }
+
+function* geradora5() {
+    yield function() {
+        console.log('Vim de um y1');
+    };
+    //o cod para aqui não continua
+    return function() {
+        console.log('Vim do return');
+    }
+
+    yield function() {
+        console.log('Vim do y2');
+    };
+}
+
+const g5 = geradora5();
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+
+func1();
+func2();
