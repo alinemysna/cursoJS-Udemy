3aee61fc0a5f6496c93f3eb61c000d7b5d5f1ff8
Aline Mysna <ianlucasgame7@gmail.com>
2024-01-22 14:45:07 -0300

    Revisão do básico em Arrays

---
+++ js/aula62/index.js
@@ -0,0 +1,26 @@
+//const nomes = ['Aline', 'Ian', 'Lucas','Israel', 'Alex'];
+//const novo = [...nomes]; // cria cópia
+//const novo = nomes; // utiliza a mesma
+//const nomes = new Array('aline', 'ian', 'lima');
+//nomes[2] = 'Lima';
+//delete nomes[2];
+//novo.pop();
+//const removido = nomes.shift();
+//const removido = nomes.pop();
+//console.log(nomes, removido);
+//console.log(novo);
+//nomes.push('Maria');
+//nomes.push('Maria','Emanuell');
+//nomes.unshift('Neide');
+//nomes.unshift('Ana');
+// const novo = nomes.slice(0, -2);
+// console.log(novo);
+
+//converter uma string numa array
+
+//const nome = 'Aline Mysna Lima';
+//const nomes = nome.split(' ');
+
+const nomes = [ 'Aline', 'Mysna', 'Lima' ];
+const nome = nomes.join(' ');
+console.log(nome);
