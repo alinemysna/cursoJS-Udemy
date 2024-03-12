5fa31ba5a4ccee2fabff03777949ec505879d8e7
Aline Mysna <ianlucasgame7@gmail.com>
2024-03-12 15:08:40 -0300

    Gerador de CPF

--- js/aula95/src/modules/ValidaCPF.js
+++ js/aula95/src/modules/ValidaCPF.js
@@ -1,48 +1,45 @@
 // 705.484.450-52 070.987.720-03
 export default class ValidaCPF {
-    constructor(cpfEnviado) {
-        Object.defineProperty(this, 'cpfLimpo', {
-            writable:false,
-            enumerable:true,
-            configurable:false,
-            value: cpfEnviado.replace(/\D+/g, '')
-        });
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviado.replace(/\D+/g, '')
+    });
+  }
 
+  éSequência() {
+    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+  }
+
+  geraNovoCpf() {
+    const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+    this.novoCPF = cpfSemDigitos + digito1 + digito2;
+  }
+
+  static geraDigito(cpfSemDigitos) {
+    let total = 0;
+    let reverso = cpfSemDigitos.length + 1;
+
+    for(let stringNumerica of cpfSemDigitos) {
+      total += reverso * Number(stringNumerica);
+      reverso--;
     }
 
-    éSequencia() {
-        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
-    }
-    
-    geraNovoCpf() {
-        const cpfSemDigito = this.cpfLimpo.slice(0, -2);
-        const digito1 = ValidaCPF.gerDigito(cpfSemDigito);
-        const digito2 = ValidaCPF.gerDigito(cpfSemDigito + digito1);
-        this.novoCpf = cpfSemDigito + digito1 + digito2;
-    }
+    const digito = 11 - (total % 11);
+    return digito <= 9 ? String(digito) : '0';
+  }
 
-    static gerDigito(cpfSemDigito) {
-        let total = 0;
-        let reverso = cpfSemDigito.length +1;
+  valida() {
+    if(!this.cpfLimpo) return false;
+    if(typeof this.cpfLimpo !== 'string') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.éSequência()) return false;
+    this.geraNovoCpf();
 
-        for(let stringNumerica of cpfSemDigito) {
-            total += reverso * Number(stringNumerica);
-            reverso--;
-        }
-        const digito = 11 - (total % 11);
-        return digito <= 9 ? String(digito) : '0';
-    }
-
-
-
-    valida() {
-        if(!this.cpfLimpo) return false;
-        if(typeof this.cpfLimpo !== 'string') return false;
-        if(this.cpfLimpo.length !== 11) return false;
-        if(this.éSequencia()) return false;
-        this.geraNovoCpf();
-
-        return this.novoCpf === this.cpfLimpo;
-    }
+    return this.novoCPF === this.cpfLimpo;
+  }
 }
-console.log(ValidaCPF);
