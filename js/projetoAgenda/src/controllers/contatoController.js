f251020311fa11de628ed2813b6b25709eab6cb6
Aline Mysna <ianlucasgame7@gmail.com>
2024-04-30 18:26:53 -0300

    Validando o front-end

--- js/projetoAgenda/src/controllers/contatoController.js
+++ js/projetoAgenda/src/controllers/contatoController.js
@@ -17,7 +17,7 @@ if(contato.errors.length > 0) {
     }
 
     req.flash('success', 'Contato registrado com sucesso.');
-    req.session.save(()=> res.redirect(`/contato/index/$(contato.contato._id)`));
+    req.session.save(()=> res.redirect(`/contato/index/${contato.contato._id}`));
     return;
 } catch(e) {
     console.log(e);
@@ -54,5 +54,15 @@ exports.edit = async function(req, res) {
         console.log(e);
         res.render('404')
     };
-   
 };
+
+exports.delete = async function(req, res) {
+    if(!req.params.id) return res.render('404');
+
+    const contato = await Contato.delete(req.params.id);
+    if(!contato) return res.render('404');
+
+    req.flash('success', 'Contato apagado com sucesso.');
+    req.session.save(()=> res.redirect('back'));
+    return;
+}
