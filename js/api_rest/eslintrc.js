1be256d0affe6ca81fcf077572f7634bdc155517
aline mysna <ianlucasgame7@gmail.com>
2024-05-29 17:41:16 -0300

    Estruturando o projeto

---
+++ js/api_rest/eslintrc.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+{
+  es6: true,
+  node: true,
+}
+extends: [
+  'airbnb-base',
+],
+globals: {
+  Atomics: 'readonly',
+  SharedArrayBuffer: 'readonly',
+},
+parserOptions: {
+  ecmaVersion: 2018,
+  sourceType: 'module',
+},
+rules: {
+  "no-console": "off",
+  "class-methods-use-this": "off",
+},
+
+};
